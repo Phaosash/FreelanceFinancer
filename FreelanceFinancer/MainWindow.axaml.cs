f6d0742328284d94af6b7a14e2ecd3b7c125ba45
Michael McKIE <michael_mckie@hotmail.com>
2026-01-05 10:33:53 +0800

    Add project files.

---
+++ FreelanceFinancer/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FreelanceFinancer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
