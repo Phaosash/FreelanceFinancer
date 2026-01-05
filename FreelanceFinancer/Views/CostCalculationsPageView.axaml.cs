4e91a2a59b64be7fe34a4b6cfb2c71cb8400b5ea
Michael McKIE <michael_mckie@hotmail.com>
2026-01-05 11:42:19 +0800

    Files missed from last commit

---
+++ FreelanceFinancer/Views/CostCalculationsPageView.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace FreelanceFinancer;
+
+public partial class CostCalculationsPageView : UserControl {
+    public CostCalculationsPageView (){
+        InitializeComponent();
+    }
+}
---
+++ FreelanceFinancer/Views/DashboardPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FreelanceFinancer;
+
+public partial class DashboardPageView : UserControl
+{
+    public DashboardPageView()
+    {
+        InitializeComponent();
+    }
+}
