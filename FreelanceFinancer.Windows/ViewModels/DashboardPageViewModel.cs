b4ad3582b5600796111b13880f99ebf1ca4c82bd
Michael McKIE <michael_mckie@hotmail.com>
2026-01-06 11:21:13 +0800

    Slight ajustments to project structure, and started adding pages for the desktop version

---
+++ FreelanceFinancer.Windows/ViewModels/DashboardPageViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace FreelanceFinancer.Windows.ViewModels;
+
+internal class DashboardPageViewModel {
+
+}
---
+++ FreelanceFinancer.Windows/Views/MainPageView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using FreelanceFinancer.Windows.ViewModels;
+using System.Windows.Controls;
+
+namespace FreelanceFinancer.Windows.Views;
+
+public partial class MainPageView : UserControl {
+    private readonly MainPageViewModel viewModel;
+
+    public MainPageView (){
+        InitializeComponent();
+        viewModel = new MainPageViewModel();
+        DataContext = viewModel;
+    }
+}
